2a6cf27fce481a7542508d07c337ab24c0c8a5d3
Mridul Rai <75464138+Mridul-Rai@users.noreply.github.com>
2021-10-10 22:16:03 +0530

    Create ReverseString.cpp

---
+++ ReverseString.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str = "geeksforgeeks";
+ 
+    // Reverse str[begin..end]
+    reverse(str.begin(), str.end());
+ 
+    cout << str;
+    return 0;
+}
