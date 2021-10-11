e7321bdd04a912f52d35497943fad333da879672
Aman Raghuwanshi <74204086+aman-ragh@users.noreply.github.com>
2021-10-11 10:52:15 +0530

    Update BFSByMatrix.cpp

--- Graph/BFSByMatrix.cpp
+++ Graph/BFSByMatrix.cpp
@@ -53,46 +53,4 @@ int main(){
     return 0;
 
 }
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define fastio()                      \
-//     ios_base::sync_with_stdio(false); \
-//     cin.tie(NULL);                    \
-//     cout.tie(NULL);
-// typedef long long ll;
-// typedef long double ld;
-// #define forall(i, a, b) for (ll i = a; i < b; i++)
-// #define en "\n"
-// #define deb(x) cout << #x << " " << x << "\n"
-// #define pb push_back
-// #define ppb pop_back
-// #define mp make_pair
-// #define eb emplace_back
-// #define bg begin()
-// #define ed end()
-// #define sz size()
-// #define vi vector<int>
-// #define yes cout<<"YES"<<en;
-// #define no cout<<"NO"<<en;
-// #define print(x) cout<<x<<en;
-// #define ff first
-// #define ss second
-// #define PI 3.141592653589793238462
 
-// void solve()
-// {
-    
-// }
-
-// int main()
-// {
-//     fastio();
-//     int t = 1;
-//     cin >> t;
-//     while (t--)
-//     {
-//         solve();
-//     }
-
-//     return 0;
-// }
