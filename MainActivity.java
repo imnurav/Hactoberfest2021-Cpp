08f13602f32e83822834abd8bdccfeff1b39c6ab
Pratibha Jain <77779435+pratibha-jain7@users.noreply.github.com>
2021-10-16 21:41:36 +0530

    Video Calling app

--- MainActivity.java
+++ MainActivity.java
@@ -1,35 +1,24 @@
-package com.example.unitconv;
+package com.example.connect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    private TextView textView;
-    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        textView = findViewById(R.id.textView2);
-        editText = findViewById(R.id.editText);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                String s = editText.getText().toString();
-                int kg = Integer.parseInt(s);
-                double pound = 2.205*kg;
-                textView.setText("The corresponding value in pound is : " + pound);
-                Toast.makeText(MainActivity.this, "Thanks for using this app", Toast.LENGTH_SHORT).show();
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
-        });
+
+        }, 2000);
     }
 }
