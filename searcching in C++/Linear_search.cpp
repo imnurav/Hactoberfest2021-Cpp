f61c7709e9fd93ef1e82529e397081f62ff27539
ROHIT KUMAR <56693260+rohit-13@users.noreply.github.com>
2021-10-19 23:25:40 +0530

    Add files via upload

--- searcching in C++/Linear_search.cpp
+++ searcching in C++/Linear_search.cpp
@@ -1,30 +1,31 @@
-// C++ code to linearly search x in arr[]. If x
-// is present then return its location, otherwise
-// return -1
-
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int search(int arr[], int n, int x)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		if (arr[i] == x)
-			return i;
-	return -1;
+void LinearSearch(int arr[], int n,int x){
+    int position=-1,left=0,right=n-1;
+    while(left<=right){
+        if(arr[left]==x){
+            position=left+1;
+            cout<<"Element found at position "<<position<<" in attemp = "<<left+1<<endl;
+        }
+        if(arr[right]==x){
+            position=right+1;
+            cout<<"Element found at position "<<position<<" in attemp = "<<n-right<<endl;
+        }
+        left++;
+        right--;
+    }
+    if (position == -1)
+        cout << "Not found in Array with "
+             << left << " Attempt";
 }
 
-// Driver code
-int main(void)
-{
-	int arr[] = { 2, 3, 4, 10, 40 };
-	int x = 10;
-	int n = sizeof(arr) / sizeof(arr[0]);
+int main(){
 
-	// Function call
-	int result = search(arr, n, x);
-	(result == -1)
-		? cout << "Element is not present in array"
-		: cout << "Element is present at index " << result;
-	return 0;
+    int arr[]={32,26,234,2,13,1,46,24};
+    int len= sizeof(arr)/sizeof(arr[0]);
+    int x=24;
+    LinearSearch(arr,len,x);
+    return 0;
+
 }
